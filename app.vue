6f3c54c5068c7a875418cbcd540ecda598a18d65
Atlenexe <hugo.mandou@gmail.com>
2024-03-30 00:25:03 +0100

    Adding Sass + fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    <NuxtWelcome />
   </div>
 </template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
+@font-face {
+  font-family: 'BagdollDisplay';
+  src: url('fonts/BagdollDisplay-Regular.woff') format('woff');
+}
+
+* {
+  font-family: inter, sans-serif;
+  font-size: 20px;
+  margin: 0;
+  padding: 0;
+}
+
+h1,
+h2 {
+  font-family: BagdollDisplay, sans-serif;
+}
+
+h1 {
+  font-size: 115px;
+  letter-spacing: 25px;
+}
+
+h2 {
+  font-size: 50px;
+  letter-spacing: 4px;
+}
+</style>
