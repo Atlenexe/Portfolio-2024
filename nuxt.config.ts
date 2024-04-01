19dc43fb52b3d2403e7655cc1c852ab736c717bc
Atlenexe <hugo.mandou@gmail.com>
2024-04-01 20:12:56 +0200

    Fix Sections location + compress noise image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
-    },
-  ],
   app: {
     baseURL: "/Portfolio-2024",
   },
